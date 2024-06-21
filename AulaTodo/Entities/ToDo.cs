6d7ae8a4a64a9322b0dfc2dfa0cc474e7c30e6a0
Elton Santos Andrade <eltonsa75@hotmail.com>
2024-06-21 11:15:39 -0300

    Criando Select com Dapper

---
+++ AulaTodo/Entities/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("Todo")]
+    public class ToDo : EntityBase<int>
+    {
+        [Required]
+        [Column("Tarefa")]
+        public string Tarefa {  get; set; }
+    }
+}
